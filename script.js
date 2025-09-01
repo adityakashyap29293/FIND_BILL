99593f382943395d8dfed68af6255aa62c1d90cd
adityakashyap29293 <kashyapaditya479@gmail.com>
2025-09-01 17:55:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const sampleBills = {
-  "8800392483": { name: "aditya", amount: "₹500", month: "march" },
-  "9811397271": { name: "khush", amount: "₹750", month : "march" },
-  "8860614053": { name: "naresh", amount: "₹100", month : "march" },
-  "7290900267": { name: "JAI", amount: "₹455", month : "march" },
-  
-};
+  "8800392483": { name: "aditya", amount: "₹500", month: "march"},
+  "9811397271": { name: "khush", amount: "₹750", month : "march"},
+  "8860614053": { name: "naresh", amount: "₹100", month : "march"},
+  "7290900267": { name: "JAI", amount: "₹455", month : "march"},
+ };
 
 function findBill() {
   const mobile = document.getElementById("mobile").value.trim();
@@ -16,11 +15,12 @@ function findBill() {
       <h3>Bill Details</h3>
       <p><strong>Name:</strong> ${bill.name}</p>
       <p><strong>Amount Due:</strong> ${bill.amount}</p>
-      <p><strong>Due Date:</strong> ${bill.due}</p>
+      <p><strong>month:</strong> ${bill.due}</p>
     `;
   } else {
     resultDiv.innerHTML = `<p style="color:red;">No bill found for this mobile number.</p>`;
   }
 }
+
 
 
